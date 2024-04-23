221b5513ca045605a6b3f0d6c29d3b8f55929d35
kadirbabaoglu <kadirbabaoglu@outlook.com>
2024-04-23 17:33:30 +0300

    initial project

---
+++ MeetingProject/MeetingProject/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using MeetingProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace MeetingProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        
+    }
+}
