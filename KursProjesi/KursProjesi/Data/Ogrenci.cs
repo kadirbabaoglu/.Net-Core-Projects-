7ca67d47722645e627d70b71f45779af013dd290
kadirbabaoglu <kadirbabaoglu@outlook.com>
2024-04-29 21:47:12 +0300

    database sınıfları eklendi.

---
+++ KursProjesi/KursProjesi/Data/Ogrenci.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KursProjesi.Data
+{
+    public class Ogrenci
+    {
+        [Key]
+        public int OgrenciId { get; set; }
+        public string? OgrenciAdi { get; set; }
+        public string? OgrenciSoyadi { get; set; }
+        public string? Eposta { get; set; }
+        public string? Telefon { get; set; }
+    }
+}
